57b4513670ee7818796ca2a725f9002b947c11cc
allthetime <chronic88@gmail.com>
2021-12-02 21:45:41 -0800

    janky snap; think up a better snap

--- grab.js
+++ grab.js
@@ -1,4 +1,5 @@
-const cc = document.getElementById('darkmode-selector');
+const pills = document.querySelectorAll('.skill-pill');
+
 
 let PRESSING = false;
 let GLOBALx = 0;
@@ -7,6 +8,7 @@ let CURRENT_ELEM = null;
 let CURRENT_START = null;
 let ANIMATION;
 let ANIMATION_COUNT = 0;
+let xDiff;
 
 document.onmousedown = () => {
     animateDraggable();
@@ -19,6 +21,8 @@ document.onmouseup = () => {
     if (PRESSING && CURRENT_ELEM) {
         PRESSING = false;
         CURRENT_ELEM.reset();
+        SWAP_ELEMENT = null;
+        xDiff = 0;
     }
 }
 
@@ -35,22 +39,106 @@ function animateDraggable() {
     ANIMATION = requestAnimationFrame(animateDraggable);
 }
 
+// function swapElements(obj1, obj2) {
+//     // create marker element and insert it where obj1 is
+//     var temp = document.createElement("div");
+//     obj1.parentNode.insertBefore(temp, obj1);
+//     // move obj1 to right before obj2
+//     obj2.parentNode.insertBefore(obj1, obj2);
+//     // move obj2 to right before where obj1 used to be
+//     temp.parentNode.insertBefore(obj2, temp);
+//     // remove temporary marker node
+//     temp.parentNode.removeChild(temp);
+//     SWAP_ELEMENT = null;
+// }
+const swapElements = function (nodeA, nodeB) {
+    // SWAP_ELEMENT = null;
+    const parentA = nodeA.parentNode;
+    const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+    // Move `nodeA` to before the `nodeB`
+    nodeB.parentNode.insertBefore(nodeA, nodeB);
+
+    // Move `nodeB` to before the sibling of `nodeA`
+    parentA.insertBefore(nodeB, siblingA);
+};
+
+// function swapElements(obj1, obj2) {
+//     // save the location of obj2
+//     var parent2 = obj2.parentNode;
+//     var next2 = obj2.nextSibling;
+//     // special case for obj1 is the next sibling of obj2
+//     if (next2 === obj1) {
+//         // just put obj1 before obj2
+//         parent2.insertBefore(obj1, obj2);
+//     } else {
+//         // insert obj2 right before obj1
+//         obj1.parentNode.insertBefore(obj2, obj1);
+
+//         // now insert obj1 where obj2 was
+//         if (next2) {
+//             // if there was an element after obj2, then insert obj1 right before that
+//             parent2.insertBefore(obj1, next2);
+//         } else {
+//             // otherwise, just append as last child
+//             parent2.appendChild(obj1);
+//         }
+//     }
+//     SWAP_ELEMENT = null;
+// }
+
+
+let SWAP_ELEMENT;
+
+
 function drawCurrentElem() {
+
     if (CURRENT_ELEM && GLOBALx && GLOBALy) {
         const d = [
             1 - (CURRENT_ELEM.state.start_point[0] - GLOBALx),
             1 - (CURRENT_ELEM.state.start_point[1] - GLOBALy),
         ];
+
+        console.log(xDiff);
+
+        if (xDiff) d[0] = d[0] - xDiff;
         CURRENT_ELEM.elem.style.transform = `translate(${d[0]}px,${d[1]}px)`
+
+        Array.from(pills).forEach(pill=>{
+
+            let target = pill.getBoundingClientRect();
+            let elem = CURRENT_ELEM.elem.getBoundingClientRect();
+
+            if (pill.innerText == CURRENT_ELEM.elem.innerText) return undefined;
+            
+            if(target.top + target.height > elem.top
+                && target.left + target.width > elem.left
+                && target.bottom - target.height < elem.bottom
+                && target.right - target.width < elem.right) 
+            {
+                if (!SWAP_ELEMENT) {
+                    SWAP_ELEMENT = pill;
+                    xDiff = target.x - elem.x;
+                    swapElements(CURRENT_ELEM.elem, pill);
+                }
+                // if (SWAP_ELEMENT && pill != SWAP_ELEMENT) {
+                //     SWAP_ELEMENT = pill;
+                //     swapElements(pill, CURRENT_ELEM.elem);
+                // }
+            }
+        });
     }
+
 }
 
 // animateDraggable();
-
+const boundaries = {};
 
 class Draggable {
 
-    constructor(element) {
+    constructor(element) { 
+        const name = element.innerText;
+        console.log(name);
         this.state = {
             mousedown: false,
             start_point: null,
@@ -60,6 +148,20 @@ class Draggable {
         // document.addEventListener('notPressing', () => {
         //     this.reset();
         // });
+
+        const bounding = element.getBoundingClientRect();
+
+            // const d = document.createElement('div');
+            // d.style.top = offsetTop + offsetHeight;
+            // d.style.left = offsetLeft;
+            // d.style.height = 10;
+            // d.style.width = 10;
+            // d.id = 'nasty';
+            // document.body.appendChild(d);
+
+
+
+        
     }
 
     mousedown = (e) => {
@@ -88,9 +190,5 @@ class Draggable {
 
 }
 
-const pills = document.querySelectorAll('.skill-pill');
 Array.from(pills).forEach(pill => new Draggable(pill));
-
-new Draggable(cc);
-
 
