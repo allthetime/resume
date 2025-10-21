cb430268a6031a39cefc66c88edac8de8a2118de
Christopher Reid <chris@onthisspot.ca>
2025-10-21 11:57:33 -0700

    make work online

--- vids.js
+++ vids.js
@@ -1,22 +1,23 @@
-const videos = [
-"https://vid.reid.systems/CQ-1.webm",
-"https://vid.reid.systems/CQ-2.webm",
-"https://vid.reid.systems/CQ-3.webm",
-"https://vid.reid.systems/CQ-4.webm",
-"https://vid.reid.systems/CQ-5.webm",
-"https://vid.reid.systems/OTS-1.webm",
-"https://vid.reid.systems/OTS-2.webm",
-"https://vid.reid.systems/OTS-3.webm",
-"https://vid.reid.systems/OTS-4.webm",
-"https://vid.reid.systems/TRIV-1.webm",
-"https://vid.reid.systems/TRIV-2.webm",
-"https://vid.reid.systems/TRIV-3.webm",
-"https://vid.reid.systems/TRIV-4.webm",
-"https://vid.reid.systems/TRIV-5.webm",
-"https://vid.reid.systems/TRIV-7.webm"
-];
+document.addEventListener('DOMContentLoaded', function() {
+    const videos = [
+    "https://vid.reid.systems/CQ-1.webm",
+    "https://vid.reid.systems/CQ-2.webm",
+    "https://vid.reid.systems/CQ-3.webm",
+    "https://vid.reid.systems/CQ-4.webm",
+    "https://vid.reid.systems/CQ-5.webm",
+    "https://vid.reid.systems/OTS-1.webm",
+    "https://vid.reid.systems/OTS-2.webm",
+    "https://vid.reid.systems/OTS-3.webm",
+    "https://vid.reid.systems/OTS-4.webm",
+    "https://vid.reid.systems/TRIV-1.webm",
+    "https://vid.reid.systems/TRIV-2.webm",
+    "https://vid.reid.systems/TRIV-3.webm",
+    "https://vid.reid.systems/TRIV-4.webm",
+    "https://vid.reid.systems/TRIV-5.webm",
+    "https://vid.reid.systems/TRIV-7.webm"
+    ];
 
-const vids_by_project = videos.reduce((acc, url) => {
+    const vids_by_project = videos.reduce((acc, url) => {
     const project = url.split('/').pop().split('-')[0];
     if (!acc[project]) {
         acc[project] = [];
@@ -258,3 +259,5 @@ function openVideoModal(videos, startIndex) {
         }
     };
 }
+
+});
